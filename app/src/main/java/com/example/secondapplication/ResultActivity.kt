5de952a772805ecd967e756d53767425c84dce2f
s18010 <s18010@std.it-college.ac.jp>
2019-11-15 18:30:04 +0900

    不正解時のResultActivityに正解を表示するよう追加

--- app/src/main/java/com/example/secondapplication/ResultActivity.kt
+++ app/src/main/java/com/example/secondapplication/ResultActivity.kt
@@ -15,19 +15,40 @@ class ResultActivity : AppCompatActivity() {
         val koala = 1
         val panda = 2
 
+
+//        val intent = Intent(this, MainActivity::class.java)
         val mainImage = intent.getIntExtra("mainImage", 0)
         val selectedAnswer = intent.getIntExtra("selectedAnswer", 0)
+        var answerText = ""
+        var isCorrect = -1
 
-        when(mainImage) {
-            chihuahua -> resultImage.setImageResource(R.drawable.chihuahua)
-            koala -> resultImage.setImageResource(R.drawable.koala)
-            panda -> resultImage.setImageResource(R.drawable.panda)
+
+        when (mainImage) {
+            chihuahua -> {
+                resultImage.setImageResource(R.drawable.chihuahua)
+                answerText = "チワワ"
+            }
+            koala -> {
+                resultImage.setImageResource(R.drawable.koala)
+                answerText = "コアラ"
+            }
+            panda -> {
+                resultImage.setImageResource(R.drawable.panda)
+                answerText = "パンダ"
+            }
+        }
+
+        when (selectedAnswer) {
+            chihuahua -> yourAnswer.text = "チワワ"
+            koala -> yourAnswer.text = "コアラ"
+            panda -> yourAnswer.text = "パンダ"
         }
 
         if (mainImage == selectedAnswer) {
-            resultText.setText(R.string.result_correct)
+            resultText.text = "正解"
         } else {
-            resultText.setText(R.string.result_incorrect)
+            resultText.text = "不正解"
+            correctAnswerText.text = "正解は「${answerText}」でした"
         }
 
         backButton.setOnClickListener {
